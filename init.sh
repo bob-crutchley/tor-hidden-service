bb0d1c336f1fb877dd308e5b81a0ac5673755eed
bob-crutchley <bob.crutchley96@gmail.com>
2019-07-28 13:27:38 +0100

    working in k8s now

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-nginx_conf="/etc/nginx/nginx.conf"
-sed -i "s/{{hidden_service_host}}/${HIDDEN_SERVICE_HOST}/g" ${nginx_conf}
-sed -i "s/{{hidden_service_port}}/${HIDDEN_SERVICE_PORT}/g" ${nginx_conf}
-mkdir -p /run/nginx
-nginx
-tor
